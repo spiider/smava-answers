5957b0448b2e1dca3413eeaf072b6f35c5232093
Hélder Pereira <blackspiider.86@gmail.com>
2017-05-27 18:53:26 +0100

    first 1hour form with basic field and some style

--- Question 3/src/App.js
+++ Question 3/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1>Register account</h1>
+        <form className="styled-form">
+          <label for="firstName">First name</label>
+          <input type="text" name="firstName" value="Max" />
+          <label for="lastName">Last name</label>
+          <input type="text" name="lastName"/>
+          <label for="email">Email</label>
+          <input type="email" name="email" value="not-a-email" />
+
+          <h2>Bank accounts</h2>
+
+          <div className="add-account"> + Add bank account</div>
+
+          <button type="submit">Submit!</button>
+        </form>
       </div>
     );
   }
